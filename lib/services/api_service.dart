849b0c8726200dc86acdfcd6ab27b6322577507f
unknown <hoangkhadooo@gmail.com>
2026-05-26 15:47:50 +0700

    Poll AI reading grading status

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,8 +4,7 @@ import 'package:universal_html/html.dart' as html;
 
 class ApiService {
   static const _buildBaseUrl = String.fromEnvironment('API_BASE_URL');
-  static const _fallbackBaseUrl =
-      'https://jeffrey-menu-seat-wonderful.trycloudflare.com';
+  static const _fallbackBaseUrl = 'https://api.thanhquangenglishcenter.io.vn';
 
   static String get baseUrl {
     final fromBuild = _normalizeBaseUrl(_buildBaseUrl);
